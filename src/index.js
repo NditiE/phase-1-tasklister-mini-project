1ef0c427afb7298295862ff081856bc3af5cbf61
MarleneElla <ellamarlene54@gmail.com>
2023-10-08 21:25:35 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+
+    const taskForm = document.getElementById ('creat-task-form');
+    const taskList = document.getElementById ('task-list');
+    const taskInput = document.getElementById ('new-task-description'); 
+
+    taskForm.addEventListener ('submit',function(i){
+      i.preventDefault();
+    
+
+    const taskDescription = taskInput.value.trim();
+    const priority = document.getElementById('priority').value;
+
+    if(taskDescription !==''){
+      const taskItem = document.createElement('lt');
+      taskItem.textContent = textDescription;
+      taskItem.classList.add(priority);
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'delete';
+      deleteButton.addEventListener('click',function(){
+        taskList.removeChild(taskItem);
+      });
+      taskItem.appendChild (deleteButton);
+
+      taskList.appendChild (taskItem);
+
+      taskInput.value = '';
+    };
+    });
 });
+ 
